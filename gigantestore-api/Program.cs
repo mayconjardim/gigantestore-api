617bc8077f15eda4cb529822a45b50f21f476a5c
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-09-08 19:47:36 -0300

    Adicionando projeto Core e Infrastructure

--- gigantestore-api/Program.cs
+++ gigantestore-api/Program.cs
@@ -13,7 +13,6 @@ namespace gigantestore_api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
